4ca3c1907f598e5e9c287a4414cd04e35328886c
Yayo27 <a01748271@itesm.mx>
2021-08-13 14:11:46 -0500

    Tarea Finalizada

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,8 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    
+    num1 = int(input("Dame un número: "))
 
+    num2 = int(input("Dame un número: "))
 
+    print("Suma: "+str((num1+num2)))
+    print("Resta: "+str((num1-num2)))
+    print("Multiplicación: "+str((num1*num2)))
 
 if __name__ == '__main__':
     main()
--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,8 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    
+    edad = int(input("Dame tu edad: "))
 
 
+    año = int(input("Dame el año actual: "))
+
+    nacimiento = año-edad
+    años_100 = nacimiento +100
+
+    print("Cumplirás 100 años en el año: "+str(años_100))
 
 
 if __name__ == '__main__':
